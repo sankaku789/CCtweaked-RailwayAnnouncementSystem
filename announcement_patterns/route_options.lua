dc293df4b94f077bed182542c8357ea738cf35ce
sankaku789 <130286575+sankaku789@users.noreply.github.com>
2026-09-19 12:53:59 +0900

    Route設定をスロット直下の構造に単純化

--- announcement_patterns/route_options.lua
+++ announcement_patterns/route_options.lua
@@ -1,10 +1,8 @@
 return {
     -- Example:
     -- ["1234567890123456789"] = {
-    --     approach = {
-    --         sample = {
-    --             "airport_access",
-    --         },
+    --     sample = {
+    --         "airport_access",
     --     },
     -- },
 }
