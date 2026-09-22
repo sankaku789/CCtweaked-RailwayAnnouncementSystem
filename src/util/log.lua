b1f04a5f13d51cd5328095cea45a9a41135ca698
sankaku789 <130286575+sankaku789@users.noreply.github.com>
2026-09-23 02:22:56 +0900

    再生タイミング調査用にログをミリ秒化

--- src/util/log.lua
+++ src/util/log.lua
@@ -1,8 +1,10 @@
 local log = {}
 
--- function: Build the legacy timestamped log prefix.
+-- function: Build the legacy timestamped log prefix with millisecond precision.
 local function prefix()
-    return "[" .. textutils.formatTime(os.time(), true) .. "] : "
+    local epochMs = os.epoch("utc")
+    local milliseconds = epochMs % 1000
+    return ("[%s.%03d] : "):format(textutils.formatTime(os.time(), true), milliseconds)
 end
 
 -- function: Write a normal log message using the legacy format.
